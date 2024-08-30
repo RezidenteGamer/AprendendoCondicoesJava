39a263d7eeb046a4fb0b23e9dfeb35f77368f6d2
Bruno Venzo Debacco <136653751+RezidenteGamer@users.noreply.github.com>
2024-08-30 01:57:29 -0300

    Atividade 9, positivo, negativo ou zero

---
+++ src/VerificacaoPositivoNegativoZero.java
@@ -0,0 +1,23 @@
+package AprendendoCondicoesJava.src;
+
+import java.util.Scanner;
+
+public class VerificacaoPositivoNegativoZero {
+    public static void main(String[] args) {
+        
+        int numero;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um número para saber se ele é positivo ou negativo ou zero: ");
+        numero = scanner.nextInt();
+
+        if(numero % 2 == 0){
+            System.out.println("O número " + numero + " é POSITIVO");
+        }else if(numero % 2 > 0){
+            System.out.println("O número " + numero + " é NEGATIVO");
+        }else{
+            System.out.println("O número inserido é ZERO");
+        }
+    }
+}
