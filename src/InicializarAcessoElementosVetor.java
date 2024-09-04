ad896529e465fd056643b8c07d0fc885f9b454b7
Bruno Venzo Debacco <136653751+RezidenteGamer@users.noreply.github.com>
2024-09-04 20:54:06 -0300

    Atividade 1 Inicialização de vetor

---
+++ src/InicializarAcessoElementosVetor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InicializarAcessoElementosVetor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int valor[] = {10, 20, 30, 40, 50};
+
+        System.out.println("O 3º valor é " + valor[2]);
+        
+    }
+}
