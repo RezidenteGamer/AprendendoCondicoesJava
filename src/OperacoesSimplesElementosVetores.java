ec9008977202f738a819b25cc71d8f7b1abce162
Bruno Venzo Debacco <136653751+RezidenteGamer@users.noreply.github.com>
2024-09-06 13:58:49 -0300

    Delete src/OperacoesSimplesElementosVetores.java

--- src/OperacoesSimplesElementosVetores.java
+++
@@ -1,9 +0,0 @@
-public class OperacoesSimplesElementosVetores {
-    public static void main(String[] args) {
-        int valores[] = {3, 6, 9, 12};
-
-        int soma = valores[0] + valores[3];
-
-        System.out.println("A soma do primeiro e último elemento é: " + soma);
-    }
-}
